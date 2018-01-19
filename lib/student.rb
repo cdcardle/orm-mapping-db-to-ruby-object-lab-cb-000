7e32218fdb48092460c656a27800c4d25e111910
Chris Cardle <cdcardle@gmail.com>
2018-01-19 15:10:37 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -105,4 +105,14 @@ class Student
 
     DB[:conn].execute(sql)
   end
+
+  def self.all_students_in_grade_X(x)
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = ?
+    SQL
+
+    DB[:conn].execute(sql, x)
+  end
 end
