538e04c9c702b11a9e25ceb6232580a6fcda7085
Chris Cardle <cdcardle@gmail.com>
2018-01-19 15:09:39 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -93,4 +93,16 @@ class Student
 
     DB[:conn].execute(sql, x)
   end
+
+  def self.first_X_students_in_grade_10
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = 10
+    ORDER BY grade ASC
+    LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql)
+  end
 end
